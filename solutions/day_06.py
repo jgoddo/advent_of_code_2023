ddb9feda626ca98e597615759de1592a8bf83ba7
jgoddo <jonas.goddon@t-online.de>
2023-12-06 06:49:28 +0100

    solve day 6

--- solutions/day_06.py
+++ solutions/day_06.py
@@ -1,19 +1,58 @@
 import aoc_helpers
+import re
 
 YEAR = 2023
 DAY = 6
 SKIP_1 = False
 
-TEST_CASES_1 = [("""""", 'FAIL')]
-TEST_CASES_2 = [("""""", 'FAIL')]
+TEST_CASES_1 = [
+    (
+        """Time:      7  15   30
+Distance:  9  40  200
+""",
+        288,
+    )
+]
+TEST_CASES_2 = [(TEST_CASES_1[0][0], 71503)]
 
 
 def part_1(part_input):
-    return 'not solved'
+    times, distances = part_input.strip().split('\n')
+    times = list(map(int, re.findall(r'\d+', times)))
+    distances = list(map(int, re.findall(r'\d+', distances)))
+
+    solution = 1
+    for time, distance in zip(times, distances):
+        wins = 0
+        for hold_duration in range(time):
+            run_duration = time - hold_duration
+            run_dist = hold_duration * run_duration
+            if run_dist > distance:
+                wins += 1
+        solution *= wins
+
+    return solution
 
 
 def part_2(part_input):
-    return 'not solved'
+    time, distance = part_input.replace(' ', '').strip().split('\n')
+    time = list(map(int, re.findall(r'\d+', time)))[0]
+    distance = list(map(int, re.findall(r'\d+', distance)))[0]
+
+    wins = 0
+
+    # run_dist = hold_duration * time - hold_duration**2
+    # find first and last number where run_dist > distance
+    # everything outside is lost
+    # everything inside is won
+
+    for hold_duration in range(time):
+        run_duration = time - hold_duration
+        run_dist = hold_duration * run_duration
+
+        if run_dist > distance:
+            wins += 1
+    return wins
 
 
 if __name__ == '__main__':
