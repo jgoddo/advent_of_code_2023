8f617b98381c2900b8d1ac65024f1086e0cba2c6
jgoddo <jonas.goddon@t-online.de>
2023-12-11 07:12:02 +0100

    add solutions

--- solutions/day_03.py
+++ solutions/day_03.py
@@ -1,19 +1,117 @@
 import aoc_helpers
+import collections
 
 YEAR = 2023
 DAY = 3
 SKIP_1 = False
 
-TEST_CASES_1 = [("""""", 'FAIL')]
-TEST_CASES_2 = [("""""", 'FAIL')]
+TEST_CASES_1 = [
+    (
+        """467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+""",
+        4361,
+    )
+]
+TEST_CASES_2 = [(TEST_CASES_1[0][0], 467835)]
 
 
 def part_1(part_input):
-    return 'not solved'
+    part_input = part_input.strip().split('\n')
+    height = len(part_input)
+    width = len(part_input[0])
+
+    numbers = set()
+    for y in range(height):
+        for x in range(width):
+            if not part_input[y][x].isdigit() and part_input[y][x] != '.':
+                # special character found
+                for y_offset in range(-1, 2):
+                    for x_offset in range(-1, 2):
+                        if (
+                            (-1 < y + y_offset < height)
+                            and (-1 < x + x_offset < width)
+                            and part_input[y + y_offset][x + x_offset].isdigit()
+                        ):
+                            numbers.add((y + y_offset, x + x_offset))
+
+    neighbour_numbers = []
+    for ny, nx in numbers:
+        res = [(ny, nx)]
+        offset = 1
+        while -1 < nx + offset < width and part_input[ny][nx + offset].isdigit():
+            res.append((ny, nx + offset))
+            offset += 1
+        offset = 1
+        while -1 < nx - offset < width and part_input[ny][nx - offset].isdigit():
+            res.append((ny, nx - offset))
+            offset += 1
+
+        res = sorted(set(res), key=lambda x: x[1])
+        if res not in neighbour_numbers:
+            neighbour_numbers.append(res)
+
+    res = 0
+    for number in neighbour_numbers:
+        curr = int(''.join([part_input[y][x] for y, x in number]))
+        res += curr
+
+    return res
 
 
 def part_2(part_input):
-    return 'not solved'
+    part_input = part_input.strip().split('\n')
+    height = len(part_input)
+    width = len(part_input[0])
+
+    solution = 0
+    for y in range(height):
+        for x in range(width):
+            if not part_input[y][x].isdigit() and part_input[y][x] == '*':
+                numbers = set()
+
+                for y_offset in range(-1, 2):
+                    for x_offset in range(-1, 2):
+                        if (
+                            (-1 < y + y_offset < height)
+                            and (-1 < x + x_offset < width)
+                            and part_input[y + y_offset][x + x_offset].isdigit()
+                        ):
+                            numbers.add((y + y_offset, x + x_offset))
+
+                if len(numbers) >= 2:
+                    neighbour_numbers = []
+                    for ny, nx in numbers:
+                        res = [(ny, nx)]
+                        offset = 1
+                        while -1 < nx + offset < width and part_input[ny][nx + offset].isdigit():
+                            res.append((ny, nx + offset))
+                            offset += 1
+                        offset = 1
+                        while -1 < nx - offset < width and part_input[ny][nx - offset].isdigit():
+                            res.append((ny, nx - offset))
+                            offset += 1
+
+                        res = sorted(set(res), key=lambda x: x[1])
+                        if res not in neighbour_numbers:
+                            neighbour_numbers.append(res)
+
+                    if len(neighbour_numbers) == 2:
+                        # excatly 2 numbers
+                        n1 = int(''.join([part_input[y][x] for y, x in neighbour_numbers[0]]))
+                        n2 = int(''.join([part_input[y][x] for y, x in neighbour_numbers[1]]))
+
+                        solution += n1 * n2
+
+    return solution
 
 
 if __name__ == '__main__':
--- solutions/day_07.py
+++ solutions/day_07.py
@@ -1,19 +1,134 @@
 import aoc_helpers
+from collections import Counter
+from functools import cmp_to_key
+import itertools
 
 YEAR = 2023
 DAY = 7
-SKIP_1 = False
+SKIP_1 = True
 
-TEST_CASES_1 = [("""""", 'FAIL')]
-TEST_CASES_2 = [("""""", 'FAIL')]
+TEST_CASES_1 = [
+    (
+        """32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483
+""",
+        6440,
+    )
+]
+TEST_CASES_2 = [(TEST_CASES_1[0][0], 5905)]
+
+
+def get_card_type(val):
+    if len(val) == 1:
+        # 5 of a kind
+        return 7
+    if max(val.values()) == 4:
+        return 6
+    if len(val.values()) == 2:
+        return 5
+    if max(val.values()) == 3:
+        return 4
+    if len(val.values()) == 3:
+        return 3
+    if len(val.values()) == 4:
+        return 2
+    return 1
+
+
+CARD_ORDER = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
+
+
+def cmp(a, b):
+    a_score = get_card_type(Counter(a))
+    b_score = get_card_type(Counter(b))
+    if a_score < b_score:
+        return -1
+    elif a_score > b_score:
+        return 1
+
+    # same score
+    for a_char, b_char in zip(a, b):
+        if a_char == b_char:
+            continue
+        if CARD_ORDER.index(a_char) > CARD_ORDER.index(b_char):
+            return -1
+        else:
+            return 1
+    return 0
 
 
 def part_1(part_input):
-    return 'not solved'
+    part_input = part_input.strip().split('\n')
+    cards, bids, rank = [], {}, []
+    for line in part_input:
+        line = line.split()
+        cards.append(line[0])
+        bids[line[0]] = int(line[1])
+
+    cards = sorted(cards, key=cmp_to_key(cmp))
+    score = sum((idx + 1) * bids[c] for idx, c in enumerate(cards))
+
+    print(cards, score)
+
+    return score
+
+
+CARD_ORDER_2 = ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J']
+
+
+def cmp_2(a, b):
+    a_cnt = Counter(a)
+    b_cnt = Counter(b)
+    a_opts, b_opts = [a], [b]
+
+    for option in itertools.combinations_with_replacement(CARD_ORDER_2[:-1], r=a_cnt['J']):
+        tmp = a
+        for idx in range(a_cnt['J']):
+            tmp = tmp.replace('J', option[idx], 1)
+        a_opts.append(tmp)
+
+    for option in itertools.combinations_with_replacement(CARD_ORDER_2[:-1], r=b_cnt['J']):
+        tmp = b
+        for idx in range(b_cnt['J']):
+            tmp = tmp.replace('J', option[idx], 1)
+        b_opts.append(tmp)
+
+    a_score = max(get_card_type(Counter(val)) for val in a_opts)
+    b_score = max(get_card_type(Counter(val)) for val in b_opts)
+
+    if a_score < b_score:
+        return -1
+    elif a_score > b_score:
+        return 1
+
+    # same score
+    for a_char, b_char in zip(a, b):
+        if a_char == b_char:
+            continue
+        if CARD_ORDER_2.index(a_char) > CARD_ORDER_2.index(b_char):
+            return -1
+        else:
+            return 1
+    return 0
 
 
 def part_2(part_input):
-    return 'not solved'
+    part_input = part_input.strip().split('\n')
+    cards, bids, rank = [], {}, []
+    for line in part_input:
+        line = line.split()
+        cards.append(line[0])
+        bids[line[0]] = int(line[1])
+
+    cards = sorted(cards, key=cmp_to_key(cmp_2))
+    score = sum((idx + 1) * bids[c] for idx, c in enumerate(cards))
+
+    print(cards, score)
+
+    return score
 
 
 if __name__ == '__main__':
