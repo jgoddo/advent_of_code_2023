92b97128d41bf6c8620b5b37bfcfbfc9a70fbae1
jgoddo <jonas.goddon@t-online.de>
2023-12-12 06:59:23 +0100

    sync

--- solutions/day_12.py
+++ solutions/day_12.py
@@ -4,12 +4,56 @@ YEAR = 2023
 DAY = 12
 SKIP_1 = False
 
-TEST_CASES_1 = [("""""", 'FAIL')]
+TEST_CASES_1 = [
+    (
+        """???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1
+""",
+        21,
+    )
+]
 TEST_CASES_2 = [("""""", 'FAIL')]
 
 
+# how many different arrangements of operational and broken springs fit the given criteria in each row.
+# def is_valid(spring_groups,)
+
+import itertools
+
+
 def part_1(part_input):
-    return 'not solved'
+    part_input = part_input.strip().split('\n')
+    solution = 0
+    for line in part_input:
+        springs, counts = line.split()
+        counts = list(map(int, counts.split(',')))
+        spring_groups = list(filter(len, springs.split('.')))
+
+        n_springs = sum(counts)  # broken springs
+        n_broken = springs.count('#')
+        n_options = springs.count('?')
+
+        # naive solution will obviously not work but had to try :)
+        broken_to_place = n_springs - n_broken
+        operational_to_place = n_options - broken_to_place
+
+        chars = '#' * broken_to_place + '.' * operational_to_place
+        for combination in set(itertools.permutations(chars, r=len(chars))):
+            test_val = springs
+            for v in combination:
+                test_val = test_val.replace('?', v, 1)
+
+            spring_groups = list(map(lambda x: x.count('#'), filter(len, test_val.split('.'))))
+
+            if len(spring_groups) == len(counts) and all(a == b for a, b in zip(spring_groups, counts)):
+                solution += 1
+
+        print(springs, spring_groups, counts, solution)
+    return solution
 
 
 def part_2(part_input):
@@ -23,6 +67,7 @@ if __name__ == '__main__':
         for test_case, solution in TEST_CASES_1:
             test_result = part_1(test_case)
             assert test_result == solution, f'{test_result} != {solution}'
+            print('test passed!')
         res_1 = part_1(puzzle_input)
         print('Solution for part 1 is: ', res_1)
         # aoc_helpers.post_answer(res_1, DAY, 1, year=YEAR)
